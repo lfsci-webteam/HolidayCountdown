7264eccc228aea233aaf9c07eba6f04a2867b19f
lfsci-webteam <lfsci-webteam@byu.edu>
2013-11-22 16:33:55 -0700

    added try/catch

--- www/js/index.js
+++ www/js/index.js
@@ -44,14 +44,19 @@ var app = {
     	listeningElement.setAttribute('style', 'display:none;');
     	receivedElement.setAttribute('style', 'display:block;');
 
-    	$('#HomeScreen').on('pagebeforeshow', function () {
+    	try {
+    		$('#HomeScreen').on('pagebeforeshow', function () {
 
-			// query the holidays web service to get all the holidays
-    		//var url = 'http://batman.byu.local/HolidayService/HolidayService.svc/rest/GetHolidays/start/1-1-2013/end/1-1-2014?callback=?';
-    		var url = 'http://lswebservices.byu.edu/holidays/HolidayService.svc/rest/GetHolidays/start/1-1-2013/end/1-1-2014?callback=?';
-	   			$.getJSON(url, null, displayHolidays); // since the data is passed in the url we pass a null string as the url.
+    			// query the holidays web service to get all the holidays
+    			//var url = 'http://batman.byu.local/HolidayService/HolidayService.svc/rest/GetHolidays/start/1-1-2013/end/1-1-2014?callback=?';
+    			var url = 'http://lswebservices.byu.edu/holidays/HolidayService.svc/rest/GetHolidays/start/1-1-2013/end/1-1-2014?callback=?';
+    			$.getJSON(url, null, displayHolidays); // since the data is passed in the url we pass a null string as the url.
 
-    	});
+    		});
+    	}
+    	catch (err) {
+    		alert(err.message);
+    	}
 
     	$.mobile.changePage("#HomeScreen", { transition: "fade" });
     },
